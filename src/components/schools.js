83fc5ecc6dcaed3b86e6c14273807f238fb7acea
PetruCioinica <cioinicapetru@gmail.com>
2020-12-01 14:28:38 +0200

    done for now

--- src/components/schools.js
+++ src/components/schools.js
@@ -1,56 +1,24 @@
-import React from "react";
+import React, { useState } from "react";
+import HsHandler from "./hshandler";
 
-class SchoolComponent extends React.Component {
-    constructor(props) {
-        super(props);
-        this.state = {
-            Hschools: [
-                {name:"Colegiul National Calistrat Hogas",people:942},
-                {name:"Colegiul National Spiru Haret",people:653},
-                {name:"Colegiul National de Agricultura si Economie",people:1364}
-            ]
-        };
-        this.increment = this.increment.bind(this);
-        this.decrement = this.decrement.bind(this);
-    }
-    increment(name) {
-        return () => {
-            let arr = this.state.Hschools.slice(0);
-            let loc = arr.find(a => a.name === name);
-            loc.people++;
-            this.setState({Hschools:arr});
-        }
-    }
-    decrement(name) {
-        return () => {
-            let arr = this.state.Hschools.slice(0);
-            let loc = arr.find(a => a.name === name);
-            loc.people--;
-            this.setState({Hschools:arr});
-        }
-    }
-    render = () => {
-        let {Hschools} = this.state;
-        return (
-            <div className="is-family-secondary subtitle my-3">
-                {Hschools.map(
-                    (item) => {
-                        return (
-                            <div className="has-text-centered">
-                                <p className="my-4">{item.name} - <strong>{item.people} de elevi</strong></p>
-                                <button className="button is-success is-rounded" onClick={this.increment(item.name)}>Adauga
-                                    elevi
-                                </button>
-                                <button className="button is-danger is-rounded" onClick={this.decrement(item.name)}>Scoate Elevi
-                                </button>
-                            </div>
-                        )
-                    }
-                )}
-            </div>
-        );
-    }
-}
+//to do
+//implementeaza un buton de add more students care sa deschida un modal
+//in modal sa fie un input in care sa scrii cati studenti sa adaugi
+//la ok se adauga studentii
 
+const SchoolComponent = () => {
+  const [hschools, setHschools] = useState([
+    { name: "HS 1", people: 942 },
+    { name: "HS 2", people: 942 },
+    { name: "HS 3", people: 942 },
+  ]);
+  return (
+    <div className="is-family-secondary subtitle my-3">
+      {hschools.map((hs) => (
+        <HsHandler hs={hs} />
+      ))}
+    </div>
+  );
+};
 
 export default SchoolComponent;
