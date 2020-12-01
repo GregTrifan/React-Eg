28e7da721e6ad726c321adc645fd109aafc2ec61
PetruCioinica <cioinicapetru@gmail.com>
2020-12-01 14:30:05 +0200

    added task

--- src/App.js
+++ src/App.js
@@ -1,19 +1,17 @@
-import './main.scss';
-import  {Navbar} from './components/navbar';
+import "./main.scss";
+import { Navbar } from "./components/navbar";
+import React from "react";
 
-const React= require('react');
-
-
+//gaseste o modalitate de a adauga sau scadea mai multi studenti.
+//uita-te la modal in antd, foloseste si un input pt numere
 
 function App() {
   return (
-      <div>
-          <Navbar/>
-          <h1 className="title has-text-centered">Index Page</h1>
-      </div>
-
+    <div>
+      <Navbar />
+      <h1 className="title has-text-centered">Index Page</h1>
+    </div>
   );
 }
-
 
 export default App;
